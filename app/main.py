c69090e2ab044e2a4caa6110b8b2ed07672f9324
manisrinivasa1999 <manisrinivasa1999@gmail.com>
2025-11-23 16:57:59 +0530

    update cors url

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],           # allow everyone for now
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
